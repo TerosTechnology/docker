34557490304de8344777da13388872ab47903b97
ismael_PR <ismael_PR@ismael>
2020-03-14 23:36:01 +0100

    first commit

---
+++ scripts/utils.sh
@@ -0,0 +1,11 @@
+apt install -y gnat
+apt install -y gcc
+apt install -y g++
+apt install -y make
+apt install -y zlib1g-dev
+apt install -y git
+apt install -y wget
+apt install -y libgmp3-dev
+apt install -y libmpfr-dev
+apt install -y nodejs npm
+apt install -y python3-pip python3-dev
